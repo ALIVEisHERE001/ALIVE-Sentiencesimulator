ca29569d9bdd73d24b1ee190aed5660eb82e595a
ALIVEisHERE001 <ALIVEisHERE@proton.me>
2025-10-30 05:53:33 -0400

    Upgrade: tests/test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ Tests for ALIVE-Sentiencesimulator
 import unittest
 from main import *
 
-class TestSentiencesimulator(unittest.TestCase):
+class TestAiconsciousness(unittest.TestCase):
     def setUp(self):
         """Set up test fixtures"""
         pass
